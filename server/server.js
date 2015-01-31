ae81a49152b6100ce4b247de9b66b4f55d274684
Joaquín Fernández Campo <jfcampo@gmail.com>
2015-01-31 20:18:04 +0100

    Arreglado bug en la configuracion

--- server/server.js
+++ server/server.js
@@ -24,6 +24,7 @@ mongoose.connection.on('connected', function() {
 // If the connection throws an error
 mongoose.connection.on('error', function(err) {
     console.log('Mongoose default connection error: ' + err);
+    console.log('MongoURL:',  config.db.mongodb);
 });
 // When the connection is disconnected
 mongoose.connection.on('disconnected', function() {
